afae1b1acbc5fbb0498096f5f5866dee43b49e00
EduardoReisUX <ducabruno09@hotmail.com>
2021-12-02 11:00:44 -0400

    add dynamic titles and global.scss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           ></link>
-          <title>Ig News</title>
         </Head>
 
         <body>
