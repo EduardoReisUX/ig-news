e711e622fed9d17f2ed4e946df1828df16b752d7
EduardoReisUX <ducabruno09@hotmail.com>
2021-12-02 14:50:49 -0400

    add SSR with stripe api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from "./styles.module.scss";
 
-export function SubscribeButton() {
+interface SubscribeButton {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButton) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
