8cdfd6a6bc84586fbe197619feea3a41c46cc477
EduardoReisUX <ducabruno09@hotmail.com>
2021-12-07 20:46:17 -0400

    add github login with next-auth@4.0.2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo ignews" />
         <nav>
-          <a className="active">Home</a>
+          <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
 
