c4a0252c64f08fb7d0cebc1213016d9189c38e3e
EduardoReisUX <ducabruno09@hotmail.com>
2021-12-23 13:17:55 -0400

    add header nextjs links and prefetch posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { SignInButton } from "../SignInButton";
+
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -7,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo ignews" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
